d9202fe364607ff7ba778611494caaf1f360d477
BSportich <benjamin.sportich@yahoo.fr>
2025-01-14 19:40:36 +0100

    Test nbv2 surface + unknown

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -267,8 +267,8 @@ bool NBV_Selector::isFrontierVoxel_TSDF_2(const Eigen::Vector3d& voxel){
 
     }
 
-
-    if(is_surface && close_unknown && close_empty){
+    if(is_surface && close_unknown){
+    //if(is_surface && close_unknown && close_empty){
       return true;
     }
     return false;
