f436910e51738b373ac4b68d6dd6effd16553ecb
BSportich <benjamin.sportich@yahoo.fr>
2025-01-15 14:13:52 +0100

    Correction

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -270,7 +270,7 @@ bool NBV_Selector::isFrontierVoxel_TSDF_2(const Eigen::Vector3d& voxel){
     //if(is_surface){
     if(is_surface && close_unknown && close_empty){
       return true;
-    //}
+    }
     return false;
   }
 
