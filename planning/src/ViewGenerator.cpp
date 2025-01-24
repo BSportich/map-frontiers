24bb9a0b4bcfdff2658ff89a56a96ba4c598c838
BSportich <benjamin.sportich@yahoo.fr>
2025-01-24 16:38:11 +0100

    Changed viewgenerator

--- planning/src/ViewGenerator.cpp
+++ planning/src/ViewGenerator.cpp
@@ -11,7 +11,7 @@ ViewGenerator::ViewGenerator(const std::string& method_name, float distance_min,
     m_distance_min = distance_min;
     m_map = map;
 
-    max_sampling = 3;
+    max_sampling = 1;
     if(distance_min > distance_max) {
 
         //throw exception
@@ -47,7 +47,7 @@ void ViewGenerator::generateViews_sphere(const std::vector<Eigen::Vector3d>& fro
             float o_y = 0 ;
             float o_z = 0 ; 
 
-            while(isFree){
+            while(!isFree){
             
                 float r = ( (static_cast<float>(rand()) / RAND_MAX) * ( m_distance_max - m_distance_min ) + m_distance_min) ;
                 float theta = ( (static_cast<float>(rand()) / RAND_MAX)  * 2 * M_PI) ;
