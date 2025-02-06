92e9b0bb1e767aafb237b9a690298c2c75c53e69
BSportich <benjamin.sportich@yahoo.fr>
2025-02-06 11:34:58 +0100

    Typo

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -779,8 +779,8 @@ int main(int argc, char** argv) {
     sys_params.p_resolution_y = 40; // high number attendu
     sys_params.p_sampling_time =1; 
 
-    sys_params.p_fov_x_ *= M_PI / 180.0;
-    sys_params.p_fov_y_ *= M_PI / 180.0;
+    sys_params.p_fov_x *= M_PI / 180.0;
+    sys_params.p_fov_y *= M_PI / 180.0;
     ///////////////
 
 
