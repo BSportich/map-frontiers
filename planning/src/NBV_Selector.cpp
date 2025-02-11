51b3c364ad431a26ecf640a4cc04879c90c0d611
BSportich <benjamin.sportich@yahoo.fr>
2025-02-11 17:00:00 +0100

    Bypass sampling

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -751,7 +751,7 @@ void NBV_Selector::tSDFCallback(const voxblox_msgs::Layer& layer_msg){
     ROS_INFO_COND(verbose_, "[TSDF callback] THERE ARE %d FRONTIERS", frontiers_set.size() );
     //sample frontiers
     //sample_subset_frontiers_shells();
-    frontiers_subset = frontiers_set ; 
+    frontiers_subset.assign(frontiers_set.begin(), frontiers_set.begin() + m_sub_sample_size_ );
     ROS_INFO_COND(verbose_, "[TSDF callback] SAMPLING");
     publish_sub_frontiers();
     ROS_INFO_COND(verbose_, "[TSDF callback] PUBLISHING SUB FRONTIERS");
