5c356603c633e6ee80b77a0c408775eca0423edc
BSportich <benjamin.sportich@yahoo.fr>
2025-02-12 15:32:27 +0100

    Fixed typo

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -754,7 +754,7 @@ void NBV_Selector::tSDFCallback(const voxblox_msgs::Layer& layer_msg){
     //sample frontiers
     sample_subset_frontiers_shells();
      //frontiers_subset.assign(frontiers_set.begin(), frontiers_set.begin() + m_sub_sample_size_ );
-    /ROS_INFO_COND(verbose_, "[TSDF callback] SAMPLING");
+    ROS_INFO_COND(verbose_, "[TSDF callback] SAMPLING");
     publish_sub_frontiers();
     ROS_INFO_COND(verbose_, "[TSDF callback] PUBLISHING SUB FRONTIERS");
 
