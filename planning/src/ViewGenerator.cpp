7709fffc31f20ee7a3815f6e378ef6ba5f08cd02
BSportich <benjamin.sportich@yahoo.fr>
2025-02-18 16:57:12 +0100

    Typo

--- planning/src/ViewGenerator.cpp
+++ planning/src/ViewGenerator.cpp
@@ -138,7 +138,7 @@ void ViewGenerator::generateViews_gradients_ESDF(const std::vector<Eigen::Vector
         if(isSafeView_bool){
 
             ViewCandidate vc = { next_voxel.x() , next_voxel.y() , next_voxel.z() , 0,0,0,0, frontier.x() , frontier.y(), frontier.z()};
-            view_candidates.push_back( vc )
+            view_candidates.push_back( vc );
 
 
         }
