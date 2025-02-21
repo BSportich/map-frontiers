51d5843aa6317077e7d60afd1017277ebf290bb1
BSportich <benjamin.sportich@yahoo.fr>
2025-02-21 13:46:14 +0100

    Correction comments

--- planning/src/ViewGenerator.cpp
+++ planning/src/ViewGenerator.cpp
@@ -330,8 +330,8 @@ bool ViewGenerator::generateview_normal(const Eigen::Vector3d& frontier, float r
         // ROS_INFO(" Distance is %f  until %f", distance, m_distance_max);
 
         
-        if we are in the correct range 
-        compute is position safe 
+        //if we are in the correct range 
+        //compute is position safe 
         if(distance > m_distance_min){
             isSafeView_bool = isSafeView(current_pos); 
         }
