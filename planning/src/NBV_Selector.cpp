25f02c530cb18cdd6515bb670198d493bb296d77
BSportich <benjamin.sportich@yahoo.fr>
2025-03-12 13:56:49 +0100

    Test EDSF print

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -363,32 +363,32 @@ void NBV_Selector::updateFrontiers(){
           frontiers_pointcloud.push_back(point);
         }
 
-        ///empty pointcloud
-        current_state = m_map.getVoxelState_TSDF(coord_3d, m_threshold_known);
-        if ( current_state == voxblox_map::VoxbloxMap::OCCUPIED ){
+        // ///empty pointcloud
+        // current_state = m_map.getVoxelState_TSDF(coord_3d, m_threshold_known);
+        // if ( current_state == voxblox_map::VoxbloxMap::OCCUPIED ){
 
-          pcl::PointXYZRGB point;
-          point.x = coord.x();
-          point.y = coord.y();
-          point.z = coord.z();
-          point.r = 0;
-          point.g = 0;
-          point.b = 0;
-          values_for_eval_pointcloud.push_back(point);
-        }
+        //   pcl::PointXYZRGB point;
+        //   point.x = coord.x();
+        //   point.y = coord.y();
+        //   point.z = coord.z();
+        //   point.r = 0;
+        //   point.g = 0;
+        //   point.b = 0;
+        //   values_for_eval_pointcloud.push_back(point);
+        // }
 
-        ///unknown pointcloud
-        if ( current_state == voxblox_map::VoxbloxMap::UNKNOWN ){
+        // ///unknown pointcloud
+        // if ( current_state == voxblox_map::VoxbloxMap::UNKNOWN ){
 
-          pcl::PointXYZRGB point;
-          point.x = coord.x();
-          point.y = coord.y();
-          point.z = coord.z();
-          point.r = 0;
-          point.g = 0;
-          point.b = 0;
-          values_for_eval_pointcloud2.push_back(point);
-        }
+        //   pcl::PointXYZRGB point;
+        //   point.x = coord.x();
+        //   point.y = coord.y();
+        //   point.z = coord.z();
+        //   point.r = 0;
+        //   point.g = 0;
+        //   point.b = 0;
+        //   values_for_eval_pointcloud2.push_back(point);
+        // }
 
       }
 
@@ -757,9 +757,9 @@ void NBV_Selector::eSDFCallback(const voxblox_msgs::Layer& layer_msg){
   } else {
     ROS_INFO_COND(verbose_, "[ESDF callback] Deserialized sucess ! ");
 
-    //visualize_voxels_ESDF();
+    visualize_voxels_ESDF();
     //ROS_INFO_COND(verbose_, "[ESDF callback] Sorted ESDF voxels ! ");
-    //publish_test_voxels(); 
+    publish_test_voxels(); 
     //ROS_INFO_COND(verbose_, "[ESDF callback] Published test voxels ! ");
 
     generate_views() ; 
