7f95e90a72bfd77f8bdacaeb0f3aafd992379a4b
BSportich <benjamin.sportich@yahoo.fr>
2025-03-13 16:15:10 +0100

    Added prints image components and angular

--- planning/src/NBV_Selector.cpp
+++ planning/src/NBV_Selector.cpp
@@ -965,15 +965,19 @@ void NBV_Selector::select_next_best_view(){
     ROS_INFO_COND(verbose_, "COUNTING FRONTIERS VIEW %d", i);
     
     ros::Time start_view_evaluation = ros::Time::now();
-    temp_value = m_view_evaluator.count_frontiers_view(visible_voxels);
+    //temp_value = m_view_evaluator.count_frontiers_view(visible_voxels);
     //temp_value = m_view_evaluator.evaluate_view_image(visible_voxels);
     temp_value = m_view_evaluator.inverseRayCast(view, frontiers_set ); 
+    ROS_INFO_COND(verbose_, "IMAGE VALUE of  %d is %f", i, temp_value);
+
 
     ros::Time end_view_evaluation = ros::Time::now();
     ros::Duration duration = end_view_evaluation - start_view_evaluation;
     ROS_INFO_COND(timer_, "[NBV_Selector][Evaluate View] %.4f s", duration.toSec());
     Eigen::Vector3d current_pos_vector( m_current_pos.x ,m_current_pos.y , m_current_pos.z );
+
     temp_value_angular = m_view_evaluator.evaluate_view_angular( view, current_pos_vector ) ; 
+    ROS_INFO_COND(verbose_, "DIST/ANGLE COST VALUE of  %d is %f", i, temp_value_angular);
     
     values_views.push_back(temp_value);
     if( temp_value > max_value_nbv){
