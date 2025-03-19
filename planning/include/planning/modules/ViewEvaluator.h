8c68e316e4796e1ad72c46f77e4676f4578af212
BSportich <benjamin.sportich@yahoo.fr>
2025-03-19 13:46:44 +0100

    [Correction] Declaration in frontier

--- planning/include/planning/modules/ViewEvaluator.h
+++ planning/include/planning/modules/ViewEvaluator.h
@@ -492,6 +492,7 @@ bool ViewEvaluator::isSurfaceFrontier_TSDF(const Eigen::Vector3d& voxel, Eigen::
   unsigned char current_state;
   bool is_surface = false;
   bool close_unknown = false;
+  Eigen::Vector3d unknown_vox;
 
   if( voxel.z() <0){ //TO CHECK
     return false;
@@ -514,7 +515,7 @@ bool ViewEvaluator::isSurfaceFrontier_TSDF(const Eigen::Vector3d& voxel, Eigen::
     if (voxel_state == voxblox_map::VoxbloxMap::UNKNOWN) {
       close_unknown = true;
       unknown_vox = voxel + c_neighbor_voxels_[i] ;
-      ROS_INFO( "FOUND UNKNOWN %f %f %f", (voxel + c_neighbor_voxels_[i]).x(), (voxel + c_neighbor_voxels_[i]).y(), (voxel + c_neighbor_voxels_[i]).z());
+      ROS_INFO( "FOUND UNKNOWN %f %f %f", unknown_vox.x(), unknown_vox.y(), unknown_vox.z());
       return true;
     }
 
