a587a1bca94dfbaca5d7ee3c52170666e3f94983
BSportich <benjamin.sportich@yahoo.fr>
2025-03-20 18:38:19 +0100

    [Modif] Same but part 2

--- planning/include/planning/modules/ViewEvaluator.h
+++ planning/include/planning/modules/ViewEvaluator.h
@@ -70,6 +70,7 @@ public:
 
 
     float evaluate_view_angular(const ViewCandidate& vc, const Eigen::Vector3d& robot_pos, const Eigen::Vector3d& vel);
+    float evaluate_distance_angle_cost( float value_angular, float dist_min_frontiers, float dist_view );
 
     void getVisibleVoxels_camera(const ViewCandidate& vc);
     void getVisibleVoxels_LIDAR(std::vector<Eigen::Vector3d>* result, const Eigen::Vector3d& position, const Eigen::Quaterniond& orientation);
@@ -231,6 +232,10 @@ float ViewEvaluator::evaluate_view_angular(const ViewCandidate& vc, const Eigen:
   result = acos( result ) ; 
   result = (M_PI - result) / M_PI ; 
   return result ;  
+}
+
+float ViewEvaluator::evaluate_distance_angle_cost( float value_angular, float dist_min_frontiers, float dist_view ){
+  return value_angular * (dist_min_frontiers / dist_view ) ;
 }
 
 
