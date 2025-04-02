8125fb70ed43cad1d2cedb4697ca6440dec3f654
kboubakr <kenza.boubakri@inria.fr>
2025-04-02 14:27:49 +0200

    [node_launcher] should also activate each mav_local_planner's voxblox server

--- planning/scripts/voxblox_node_launcher.py
+++ planning/scripts/voxblox_node_launcher.py
@@ -8,6 +8,7 @@ class NodeLauncher:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('node_launcher', anonymous=True)
+        rospy.loginfo("[node_launcher] Starting node ...")
         self._rate = rospy.Rate(0.1)
         self._has_called_services = False
 
@@ -16,7 +17,7 @@ class NodeLauncher:
         # FIXME: this architecture forces the param /drone_name to be already loaded to the rosparams
         drone_name = rospy.get_param('/drone_name')
         self._topic_namespace = "/" + drone_name + "_"
-        self._service_name = rospy.get_param('~service_name', "start_NBV_selector")
+        self._services_name = rospy.get_param('~service_name', ["start_NBV_selector", "mav_local_planner/activate_node"])
         self._number_of_drones = rospy.get_param('/number_of_drones')
 
         # Service server
@@ -29,31 +30,33 @@ class NodeLauncher:
         voxblox_service_name = "/voxblox_node/activate_node"
         rospy.wait_for_service(voxblox_service_name)
         self._service_clients.append(rospy.ServiceProxy(voxblox_service_name, Empty))
+        rospy.loginfo("[node_launcher] Added service /voxblox_node/activate_node to the list of services to call")
 
         # Should work for N number of service
         for drone_id in range(self._number_of_drones):
-            full_service_name = self._topic_namespace + str(drone_id) + '/' + self._service_name
-            # Wait for the service to be available
-            rospy.wait_for_service(full_service_name)
-            rospy.loginfo("[node_launcher] Added service %s to the list of services to call", full_service_name)
-            self._service_clients.append(rospy.ServiceProxy(full_service_name, Empty))
-            
+            for service_name in self._services_name:
+                full_service_name = self._topic_namespace + str(drone_id) + '/' + service_name
+                # Wait for the service to be available
+                rospy.wait_for_service(full_service_name)
+                rospy.loginfo("[node_launcher] Added service %s to the list of services to call", full_service_name)
+                self._service_clients.append(rospy.ServiceProxy(full_service_name, Empty))
 
     def _LaunchVoxbloxNodeCallback(self, req):
         self._CallServiceClients()
-        rospy.loginfo("[node_launcher] Done calling services")
         return []
     
     def _CallServiceClients(self):
         for client in self._service_clients:
             try:
                 client() # Call the service
+                self._has_called_services = True
             except rospy.ServiceException as e:
                 rospy.logerr("Service call failed: %s" % e)
 
     def Spin(self):
         while not rospy.is_shutdown():
             if self._has_called_services:
+                rospy.loginfo("[node_launcher] Services were called, shutting down the node.")
                 rospy.signal_shutdown("Shutdown initiated from signal.")
             else:
                 self._rate.sleep()
