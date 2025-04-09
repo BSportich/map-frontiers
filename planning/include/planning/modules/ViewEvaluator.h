6ffca7b6fec8bccae9fdc48b1f34b300ba290817
BSportich <benjamin.sportich@yahoo.fr>
2025-04-09 13:56:23 +0200

    Typo

--- planning/include/planning/modules/ViewEvaluator.h
+++ planning/include/planning/modules/ViewEvaluator.h
@@ -244,7 +244,7 @@ float ViewEvaluator::evaluate_view_angular(const ViewCandidate& vc, const Eigen:
 
 float ViewEvaluator::evaluate_distance_angle_cost( float value_angular, float dist_min_frontiers, float dist_view ){
   //return value_angular * (dist_min_frontiers / dist_view ) ;
-  return value_angular * exp( - ( dist_view ) )
+  return value_angular * exp( - ( dist_view ) ) ; 
 }
 
 
