526dc5ade0a82ab10d78ab0878e1eae5e1486839
BSportich <benjamin.sportich@yahoo.fr>
2025-04-09 17:43:57 +0200

    Correction error in print

--- planning/include/planning/modules/ViewEvaluator.h
+++ planning/include/planning/modules/ViewEvaluator.h
@@ -417,7 +417,7 @@ float ViewEvaluator::inverseRayCast(const ViewCandidate& vc,const std::vector<Ei
       if( lineOfSightCheck(vc_2, nb_unknown_vox) ){
         result = result +1 ;
         temp_distance = (frontier - pos).norm() ;
-        voxel_distance = temp_distance / m_map.getVoxelSize() ;
+        voxel_distance = temp_distance * m_map.getVoxelSize() ;
         average_dist_frontier = average_dist_frontier + temp_distance ; 
 
         if ( m_occlusion ){
